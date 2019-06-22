3ee7e70d491ef2acc1d4c3d74d24a22b9287edfb
Will <0920076@utp.edu.pe>
2019-06-22 16:33:22 -0500

    first commit

---
+++ WebArtefactos/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebArtefactos.Models;
+
+namespace WebArtefactos.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        bdArtefactoEntities1 db = new bdArtefactoEntities1();
+
+        public ActionResult Index()
+        {
+            return View(db.Artefacto.ToList());
+        }
+
+        public ActionResult getClients(string codArt, int price)
+        {
+            ViewBag.cod = codArt;
+            ViewBag.pri = price;
+            return View(db.Clientes.ToList());
+        }
+
+        public ActionResult Compra(string codi, int pric)
+        {
+           
+            return View(db.Clientes.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebArtefactos/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebArtefactos.MvcApplication" Language="C#" %>
